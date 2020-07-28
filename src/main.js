0cb1298cb120d730c2cc54f6c446e45666bff3ef
lyp <zyx_2017@163.com>
2020-07-28 08:22:44 +0800

    搭建一个测试页面-结果编译报错

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import echarts from 'echarts'
+
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$echarts = echarts;
+
+echarts.registerMap('world',world);
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@views/test'
+
+const routerSet = {
+  routes: [{
+    path: '/test',
+    name:'Test',
+    component: Test
+  }]
+}
+
+/* let boot = window.location.origin
+if (boot.indexOf('localhost') != -1) {
+  routerSet.routes.push({
+    path: '/',
+    component: testHomePage
+  })
+} */
+
+
 
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
+export default new Router(routerSet);
+/* export default new Router({
+  routes: [{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: test
     }
   ]
-})
+}) */
